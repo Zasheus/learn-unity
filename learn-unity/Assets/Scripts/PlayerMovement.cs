748c6ae98167bb5b66fa5f3f583e8148cc55dda0
Ryan Mardiono <ryanmardiono@gmail.com>
2021-07-15 01:30:13 +1200

    added movement for left and right force on script playermovement

--- learn-unity/Assets/Scripts/PlayerMovement.cs
+++ learn-unity/Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     rb.AddForce(0, 200, 500);
-    // }
+    public int forwardForce = 1000;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("a")) 
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d")) 
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
     }
 }
